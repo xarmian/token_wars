a20b28c2468f1dcb91a491002a19e99ed972342a
David Runion <david.runion@imedemr.com>
2024-01-19 06:00:34 -0500

    update to use indexer

--- capture_vsa.js
+++ capture_vsa.js
@@ -1,45 +1,28 @@
 import { algodClient, indexerClient, db } from './config';
 import * as database from './database.js';
 
-const startRound = 35097494;
-
 (async () => {
-    console.log("Starting captureData...");
-    //let round = await algodClient.status().do();
-    //let currentRound = round["last-round"];
-    let currentRound = startRound;
+    // get list of tokens from tokens table
+    let assetIds = database.getTokens(db,'VSA').map(token => token.token_id);
 
-    while (true) {
-        let block = await indexerClient.lookupBlock(currentRound).do();
-        if (!block || !block['transactions']) {
-            console.log("Waiting for transactions...");
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            continue;
+    for (let assetId of assetIds) {
+        const startRound = database.getStartRound(db, assetId);
+        console.log(`Starting captureData for asset ${assetId} at round ${startRound}...`);
+
+        let response = await indexerClient.searchForTransactions()
+                                          .assetID(asaId)
+                                          .minRound(startRound)
+                                          .do();
+
+        for (let tx of response.transactions) {
+            let fromWallet = await algodClient.accountInformation(tx.sender).do();
+            let fromBalance = fromWallet["amount"];
+
+            let toWallet = await algodClient.accountInformation(tx.receiver).do();
+            let toBalance = toWallet["amount"];
+
+            database.writeTransaction(db, { transaction_id: tx.id, token_id: assetId, amount: tx.amount, from_wallet: tx.sender, to_wallet: tx.receiver, from_balance: fromBalance, to_balance: toBalance, block: tx.confirmedRound, timestamp: tx.timestamp });
+
         }
-        
-        for (let txn of block.transactions) {
-            //if (txn["asset-transfer-transaction"] === undefined) continue;
-            //if (txn["asset-transfer-transaction"]["asset-id"] === undefined) continue;
-
-            if (assetIds.includes(txn["asset-id"])) {
-                let transactionId = txn["id"];
-                let tokenId = txn["asset-index"];
-                let amount = txn["asset-amount"];
-                let timestamp = block["timestamp"];
-                let fromWallet = txn["sender"];
-                let toWallet = txn["payment-transaction"]["receiver"];
-
-                // use algodClient to get balances at block height for from and to wallets
-                let fromWalletRec = await algodClient.accountInformation(fromWallet).do();
-                let fromBalance = fromWalletRec["amount"];
-
-                let toWalletRec = await algodClient.accountInformation(toWallet).do();
-                let toBalance = toWalletRec["amount"];
-                
-                database.writeTransaction(db, { transaction_id: transactionId, token_id: tokenId, amount, timestamp, from_wallet: fromWallet, to_wallet: toWallet, from_balance: fromBalance, to_balance: toBalance, block: currentRound });
-            }
-        }
-        currentRound++;
-        break;
     }
 })();
